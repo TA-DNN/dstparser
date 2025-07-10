611d81ca7114837aee280bd8677400b2c02b18eb
Anton Prosekin <ayprosekin@gmail.com>
2025-07-10 07:04:46 +0000

    fix xxyy id for TAx4

--- src/dstparser/__init__.py
+++ src/dstparser/__init__.py
@@ -1,11 +1,13 @@
 from importlib.metadata import version
 from dstparser.dst_adapter import parse_dst_file
+from dstparser.dst_adapter_tax4 import parse_dst_file_tax4
 
 __version__ = version("dstparser")
 
 
 __all__ = [
     "parse_dst_file",
+    "parse_dst_file_tax4",
     "dst_adapter",
     "dst_parsers",
     "dst_reader",
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,11 @@
 import numpy as np
-from dstparser import parse_dst_file
+from dstparser import parse_dst_file, parse_dst_file_tax4
 from time import time
 from dstparser.cli.cli import parse_config
 import sys
 
 
-def test_parser(dst_file, print_read_data=False):
+def test_parser(dst_file, print_read_data=False, use_ta_x4 = False):
 
     if len(sys.argv) > 1:
         config = parse_config(sys.argv[1])
@@ -17,8 +17,13 @@ def test_parser(dst_file, print_read_data=False):
     # is parse_dst_file
     # paths to the directories with data is dstparser.paths module
     # data = parse_dst_file(dst_file, up_low_traces=True)
+    
+    if use_ta_x4:
+        convert_func = parse_dst_file_tax4
+    else:
+        convert_func = parse_dst_file
 
-    data = parse_dst_file(
+    data = convert_func(
         dst_file,
         ntile=7,
         xmax_reader=None,
@@ -30,6 +35,7 @@ def test_parser(dst_file, print_read_data=False):
 
     end = time()
     print(f"Parse time: {end - start:.3f} sec")
+
 
     if print_read_data:
         print(f"\nConverted arrays:\n---")
@@ -53,9 +59,16 @@ def test_parser(dst_file, print_read_data=False):
 
 
 if __name__ == "__main__":
-    dst_file = (
-        "/ceph/work/SATORI/projects/TA-ASIoP/INR_group/cluster82/grisha/tasdmc_SIBYLL_fe/p2/"
-        "DAT013520.corsika77420.SIBYLL.tar.gz.spctr1.1945.noCuts.dst.gz"
-    )
+    # dst_file = (
+    #     "/ceph/work/SATORI/projects/TA-ASIoP/INR_group/cluster82/grisha/tasdmc_SIBYLL_fe/p2/"
+    #     "DAT013520.corsika77420.SIBYLL.tar.gz.spctr1.1945.noCuts.dst.gz"
+    # )
+    # dst_file = "/ceph/work/SATORI/projects/TA-ASIoP/tasdmc_dstbank/qgsii04proton/080417_160603/Em1_bsdinfo/DAT055402_gea.rufldf.dst.gz"
+    
+    dst_file = "/ceph/work/SATORI/projects/TA-ASIoP/tasdmc_dstbank/tax4/qgsii04proton/north/221101to240124/DAT000010_gea.rufldf.dst.gz"
+    
     # dst_file = "/ceph/work/SATORI/projects/TA-ASIoP/tasdmc_dstbank/qgsii04nitrogen/080417_160603/Em1_bsdinfo/DAT081325_gea.rufldf.dst.gz"
-    test_parser(dst_file, print_read_data=True)
+    # !If you want to use TAx4 format, set use_ta_x4=True
+    # !If you want to use TA format, set use_ta_x4=False
+    # !By default, use_ta_x4=False
+    test_parser(dst_file, print_read_data=True, use_ta_x4 = True)
